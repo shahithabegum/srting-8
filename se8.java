289922faf77a2f0626154e2e8556215a52494604
shahithabegum <69446892+shahithabegum@users.noreply.github.com>
2021-11-11 15:37:13 +0530

    Add files via upload

---
+++ se8.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+class se8
+{
+  public static void main(String arg[])
+   {
+     try
+         {
+            String s="this is excerise";
+            
+	    System.out.println(s.hashCode());
+	  
+	}
+	catch(Exception e)
+	{
+	  System.out.println(e);
+	}
+       }
+}
